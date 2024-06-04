92c0e4a4314c101f1ec35c1ebfd669660b9b2cf5
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2024-06-04 16:08:11 +0530

    initialised development

--- app/src/main/java/com/devrachit/kotlineary/MainActivity.kt
+++ app/src/main/java/com/devrachit/kotlineary/MainActivity.kt
@@ -20,28 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             KotlinearyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text(text = "Hello, World!", modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinearyTheme {
-        Greeting("Android")
-    }
-}
