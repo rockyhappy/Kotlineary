b4e4ffef272fb14fee6600e1f6c99f0b165478df
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2024-06-05 10:35:02 +0530

    Main Screen Cards Implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
 
     // For compose navigation testing
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.0")
---
+++ app/src/main/java/com/devrachit/kotlineary/presentation/Screens/HomeScreen/components/RecipeCardPopular.kt
@@ -0,0 +1,101 @@
+package com.devrachit.kotlineary.presentation.Screens.HomeScreen.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.devrachit.kotlineary.common.Constants.CustomFontFamily
+
+@ExperimentalMaterial3Api
+@Composable
+fun RecipeCardPopular(subtitle:String,title:String , imageUrl:String , onClick:()->Unit) {
+    var sizeImage by remember { mutableStateOf(IntSize.Zero) }
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color.Transparent, Color.Black),
+        startY = sizeImage.height.toFloat()/3,  // 1/3
+        endY = sizeImage.height.toFloat()
+    )
+    Box(
+        modifier= Modifier
+            .height(200.dp)
+            .width(200.dp),
+        contentAlignment = Alignment.BottomStart
+    ){
+        //Image
+        //Title
+        Card(
+            onClick = {onClick.invoke()},
+            modifier = Modifier.fillMaxSize(),
+            shape = RoundedCornerShape(16.dp),
+            elevation = CardDefaults.cardElevation(),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                //Image
+                //Title
+                val painter= rememberAsyncImagePainter(model =imageUrl)
+                Image(painter = painter, contentDescription = null, modifier = Modifier.onGloballyPositioned {
+                    sizeImage = it.size
+                })
+                Box(modifier = Modifier.matchParentSize().background(gradient))
+                Text(
+                    text = title,
+                    color = Color.White,
+                    fontFamily = CustomFontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize=16.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.padding(start=16.dp, bottom=30.dp)
+                )
+                Text(
+                    text = subtitle,
+                    color = Color.Gray,
+                    fontFamily = CustomFontFamily,
+                    fontSize=12.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    modifier = Modifier.padding(start=16.dp, bottom=10.dp)
+                )
+            }
+
+
+        }
+    }
+
+}
+
+@ExperimentalMaterial3Api
+@Preview
+@Composable
+fun RecipeCardPopularPreview() {
+    RecipeCardPopular(subtitle = "Ready in 25 min", title = "Shahi Paneer", imageUrl = "https://via.placeholder.com/200", onClick = {})
+}
